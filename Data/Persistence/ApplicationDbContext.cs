8daf2afbe67c428c43e76712bc718b4a4cf5f616
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-06-04 11:44:07 -0600

    Add files via upload

---
+++ Data/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,73 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Persistence
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //se definen las tablas de la base de datos, con eso se crea por defecto con los datos que tiene person entity definidos
+        public DbSet<personEntity> Persons { get; set; }
+
+        //esto es para darlo especifico, se le dice que la tabla se llamara Persons, sino se le dice nada se llamaria personEntity
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+            modelBuilder.Entity<personEntity>(entity =>
+            {
+                entity.ToTable("Persons");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id).IsRequired().ValueGeneratedOnAdd();
+
+                entity.Property(e => e.Code).IsRequired().HasMaxLength(20);
+                entity.HasIndex(e=> e.Code).IsUnique();
+
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(50);
+
+                entity.Property(e=>e.LastName).IsRequired().HasMaxLength(50);
+
+                entity.Property(e=>e.Email).IsRequired().HasMaxLength(100);
+
+                entity.Property(e=>e.PhoneNumber).IsRequired().HasMaxLength(15);
+
+                entity.Ignore(e => e.FullName);
+                
+                entity.Property<DateTime>("CreateAt").IsRequired().HasDefaultValueSql("GETUTCDATE()");
+
+                entity.Property<DateTime>("UpdatedAt").IsRequired().HasDefaultValueSql("GETUTCDATE()");
+
+            });
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken= default)
+        {
+            //extras
+            UpdateTimeStamps();
+            return base.SaveChangesAsync(cancellationToken);
+
+        }
+
+        private void UpdateTimeStamps()
+        {
+            var entries = ChangeTracker.Entries()
+                .Where(e=> e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                if (entry.Metadata.FindProperty("UpdatedAt")!=null){
+                    entry.Property("UpdatedAt").CurrentValue = DateTime.UtcNow;
+                } 
+                
+            }
+        }
+    }
+}
---
+++ Data/Repositories/PersonRepository.cs
@@ -0,0 +1,95 @@
+﻿using Data.Persistence;
+using Domain;
+using Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data.Repositories
+{
+    public class PersonRepository : IRepository<personEntity, Guid>, ICodeRepository<personEntity>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //aqui se hace el como se va a hacer, esta primera funcion como se va a realizar en GetByIdAsync establecido en IRePOSITORY
+        public async Task<personEntity?> GetByIdAsync(Guid id)
+        {
+            return await _context.Persons.FirstOrDefaultAsync(p => p.Id == id);
+
+        }
+
+        public async Task<IEnumerable<personEntity>> GetAllAsync()
+        {
+            //Estas serian como las consultas sql
+            return await _context.Persons
+                .AsNoTracking() //desabilita rastreo para que sea mas rapido
+                .OrderBy(p => p.FirstName)
+                .ThenBy(p => p.LastName)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(personEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await _context.Persons.AddAsync(entity);
+        }
+
+        public Task UpdateAsync(personEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            _context.Persons.Update(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(personEntity entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            _context.Persons.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        //IcodeRepository 
+        public async Task<personEntity?> GetByCodeAsync(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentException("el codigo no puede estar vasio", nameof(code));
+            }
+            var normalizedCode = code.ToUpperInvariant();
+
+            return await _context.Persons.FirstOrDefaultAsync(p => p.Code == normalizedCode);
+        }
+
+        public async Task<bool> ExistWithCodeAsync(string code)
+        {
+            if (string.IsNullOrWhiteSpace(code))
+            {
+                throw new ArgumentException($"{code} El codigo no puede estar vazio", nameof(code));
+            }
+            var normalizedCode = code.ToUpperInvariant();
+            return await _context.Persons.AnyAsync(p => p.Code == normalizedCode);// AnyAsync devuelve true si encuentra al menos un registro que cumpla la condicion, de lo contrario devuelve false
+        }
+    }
+}
