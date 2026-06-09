3d4214002daa7c922e4b65d3bb0186d21840968b
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-06-08 21:31:18 -0600

    Creacion entidad visita

---
+++ Domain/Abstractions/IVisitRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Abstractions
+{
+    public interface IVisitRepository<TEntity> where TEntity : class
+    {
+        Task<bool> HasActiveVisitAsync(Guid personId);
+        Task<IEnumerable<TEntity>> GetActiveVisitsAsync();
+        Task<IEnumerable<TEntity>> GetVisitasByPersonIdAsync(Guid personId);
+    }
+}
---
+++ Domain/VisitEntity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class VisitEntity
+    {
+        public Guid Id { get; private set; }
+        public Guid PersonId { get; private set; }
+        public DateTime EntryTime { get; private set; }
+        public DateTime? ExitTime { get; private set; }
+
+        public personEntity? Person { get; private set; }
+
+        public bool isActive => ExitTime == null;
+
+        public TimeSpan? Duration => ExitTime.HasValue ? ExitTime.Value - EntryTime : null;
+
+        public VisitEntity(Guid personId,DateTime? entryTime=null)
+        {
+            if(personId==Guid.Empty)
+            {
+                throw new ArgumentException("El id de la persona esta vacio",nameof(personId));
+            }
+
+            Id = Guid.NewGuid();
+            PersonId = personId;
+            EntryTime = entryTime ?? DateTime.UtcNow; // el doble ?? significa que si lo del lado izquierdo es null se asigna lo del derecho
+            ExitTime = null;
+        }
+
+        public void RegisterExit(DateTime? exitTime=null)
+        {
+            var exit = exitTime ?? DateTime.UtcNow;
+
+            if (ExitTime.HasValue)
+            {
+                throw new InvalidOperationException("Esta visita ya tiene una salida registrada");
+            }
+
+            if (exit <= EntryTime)
+            {
+                throw new ArgumentException("La salida debe ser despues de la entrada",nameof(exitTime));
+            }
+
+            ExitTime = exit;
+        }
+    }
+}
