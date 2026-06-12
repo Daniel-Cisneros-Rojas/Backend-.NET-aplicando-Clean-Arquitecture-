4eb75f44bfc724d41cd065cd8dd5507da4ae6bed
Daniel Cisneros Rojas <60678815+Daniel-Cisneros-Rojas@users.noreply.github.com>
2026-06-12 12:45:15 -0600

    Tablas para visit

--- Data/Persistence/ApplicationDbContext.cs
+++ Data/Persistence/ApplicationDbContext.cs
@@ -15,6 +15,7 @@ namespace Data.Persistence
 
         //se definen las tablas de la base de datos, con eso se crea por defecto con los datos que tiene person entity definidos
         public DbSet<personEntity> Persons { get; set; }
+        public DbSet<VisitEntity> Visits { get; set; }
 
         //esto es para darlo especifico, se le dice que la tabla se llamara Persons, sino se le dice nada se llamaria personEntity
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -44,6 +45,44 @@ namespace Data.Persistence
                 entity.Property<DateTime>("CreateAt").IsRequired().HasDefaultValueSql("GETUTCDATE()");
 
                 entity.Property<DateTime>("UpdatedAt").IsRequired().HasDefaultValueSql("GETUTCDATE()");
+
+            });
+
+            modelBuilder.Entity<VisitEntity>(entity =>
+            {
+                entity.ToTable("Visits");
+                
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e=> e.Id).IsRequired().ValueGeneratedOnAdd();
+
+                entity.Property(e=>e.PersonId).IsRequired();
+
+                entity.Property(e => e.EntryTime).IsRequired();
+
+                entity.Property(e => e.ExitTime).IsRequired();
+
+                entity.Ignore(e => e.isActive);
+
+                entity.Ignore(e => e.Duration);
+
+                entity.HasOne(e => e.Person)  //una visita es hecha solo por una persona
+                .WithMany()                    //una persona puede hacer varias visitas
+                .HasForeignKey(e=>e.PersonId)
+                .OnDelete(DeleteBehavior.Restrict); //no puedes borrar a la persona si tiene visitas
+
+                entity.HasIndex(e => e.PersonId);  //definicion de indices para busquedas mas rapidas, solo en campos necesarios
+                entity.HasIndex(e => e.EntryTime);
+                entity.HasIndex(e => new {e.PersonId, e.ExitTime}); //indice compuesto 
+
+                entity.Property<DateTime>("CreatedAt")
+                .IsRequired()
+                .HasDefaultValue("GETUTCDATE()");
+
+                entity.Property<DateTime>("UpdatedAt")
+                .IsRequired()
+                .HasDefaultValue("GETUTCDATE()");
+
 
             });
         }
